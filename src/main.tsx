25006d4c4f460a527acb6ffe28d69e5a01ceaf4c
Srikanth <srikanthraj.brchubllp@gmail.com>
2026-02-14 21:57:59 +0530

    downgraded react to stable v18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client' // <--- Note the '/client'
 import App from './App.tsx'
 import './index.css'
-import { HelmetProvider } from 'react-helmet-async'; // <--- Import this
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider> {/* <--- Wrap App with this */}
+    <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>,
